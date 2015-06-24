09b6d417d8e896ed7f301d5b621535b8f44c3cf5
takashi-f <t.furuno@betasoft.co.jp>
2015-06-24 14:34:25 +0900

    spec3: ファイルからの読み込み機能を追加しました。
    spec1 の「オムライス」の文字列を返すだけの部分は可読性を上げるために削除しました。

--- src/ReadableCode.java
+++ src/ReadableCode.java
@@ -1,13 +1,22 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class ReadableCode {
 
 	/**
 	 * [概要]プログラムのエントリーポイント。
 	 * @param args データファイル名を指定する。
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		PrintInRecipeBase recipePrinter = new PrintInOmeletteRice();
+		String recipePath = "src/recipe-data.txt";
+		
+		PrintInRecipeBase recipePrinter = new PrintInRecipeText(recipePath);
 
 		recipePrinter.PrintIn();
 
@@ -17,47 +26,47 @@ public class ReadableCode {
 
 abstract class PrintInRecipeBase
 {
-	abstract protected String CreateRecipe();
+	abstract protected String createRecipe() throws FileNotFoundException, IOException;
 
-
-	public void PrintIn()
+	public void PrintIn() throws IOException
 	{
-		PrintIn(CreateRecipe());
+		PrintIn(createRecipe());
 	}
 
-
-	private void PrintIn(String output)
+	protected void PrintIn(String output)
 	{
 		System.out.println(output);
 	}
 }
 
-
-class PrintInOmeletteRice extends PrintInRecipeBase
-{
-	protected String CreateRecipe()
-	{
-		return "オムライス";
-	}
-
-}
-
-
 class PrintInRecipeText extends PrintInRecipeBase
 {
-
 	private String recipePath = "";
 
 	public PrintInRecipeText(String recipeTextPath)
 	{
 		recipePath = recipeTextPath;
-
 	}
 
-	protected String CreateRecipe()
+	protected String createRecipe() throws IOException
 	{
-		return "目下工事中";
+		ReadFile readFile = new ReadFile();
+		File recipeFile = readFile.readRecipe(recipePath);
+		
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(recipeFile));
+
+		String row;
+		String allText = "";
+		while((row = bufferedReader.readLine()) != null) {
+			allText = allText + row;
+		}
+		bufferedReader.close();
+		return allText;
 	}
+}
 
-
+class ReadFile {
+	public File readRecipe(String readRecipeFilePath) {
+		return new File(readRecipeFilePath);
+	}
 }
