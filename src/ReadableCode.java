09b1c71b3d868667d4b0367d0d7d19806113d179
Takajun780 <Takajun780@gmail.com>
2015-06-24 13:18:39 +0900

    spec3: 仕様3実装途中。まだ動作しません。

--- src/ReadableCode.java
+++ src/ReadableCode.java
@@ -1,13 +1,63 @@
 
 public class ReadableCode {
 
-	// [概要]プログラムのエントリーポイント。
+	/**
+	 * [概要]プログラムのエントリーポイント。
+	 * @param args データファイル名を指定する。
+	 */
 	public static void main(String[] args) {
 
-		System.out.println("オムライス");
+		PrintInRecipeBase recipePrinter = new PrintInOmeletteRice();
+
+		recipePrinter.PrintIn();
 
 	}
 
 }
 
+abstract class PrintInRecipeBase
+{
+	abstract protected String CreateRecipe();
 
+
+	public void PrintIn()
+	{
+		PrintIn(CreateRecipe());
+	}
+
+
+	private void PrintIn(String output)
+	{
+		System.out.println(output);
+	}
+}
+
+
+class PrintInOmeletteRice extends PrintInRecipeBase
+{
+	protected String CreateRecipe()
+	{
+		return "オムライス";
+	}
+
+}
+
+
+class PrintInRecipeText extends PrintInRecipeBase
+{
+
+	private String recipePath = "";
+
+	public PrintInRecipeText(String recipeTextPath)
+	{
+		recipePath = recipeTextPath;
+
+	}
+
+	protected String CreateRecipe()
+	{
+		return "目下工事中";
+	}
+
+
+}
